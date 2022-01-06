cbfa912a55210b7a3c426626f256d91cf40a6289
Megan McLean <425megs@gmail.com>
2022-01-06 10:01:48 -0800

    high score tables working

--- board.js
+++ board.js
@@ -1,6 +1,25 @@
 var currentHighScores = []
 var scoreTable = document.querySelector("#table")
 
+
+function renderScores(){
+    currentHighScores.sort(function(a,b) {
+        return b[1] - a[1];
+    })
+
+    for (var i = 0; i < currentHighScores.length; i++) {
+        var score = currentHighScores[i];
+        var tdInitial = document.createElement("td");
+        var tdScore = document.createElement("td");
+        var tr = document.createElement("tr");
+        tdInitial.textContent = score[0];
+        tdScore.textContent = score[1];
+        tr.appendChild(tdInitial);
+        tr.appendChild(tdScore);
+        scoreTable.appendChild(tr);
+    }
+}
+
 function initialize(){
     if (localStorage.getItem("highScores") !==null) {
         currentHighScores=JSON.parse(localStorage.getItem("highScores"));
@@ -16,21 +35,9 @@ function initialize(){
     renderScores();
 }
 
-function renderScores(){
-    currentHighScores.sort(function(a,b) {
-        return b[1] - a[1];
-    })
-
-    for (var i = 0; i < currentHighScores.length; i++) {
-        var score = currentHighScores[i];
-        var td = document.createElement("td");
-        td.textContent = "  " + score[0];
-        score.appendChild(td);
-    }
-}
-
 function clearBoard() {
     localStorage.removeItem("newScore");
+    localStorage.clear;
 }
     
 initialize();
